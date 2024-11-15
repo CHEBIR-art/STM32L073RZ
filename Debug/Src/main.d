7e645d15e1a86a7dad5995580f3f6c8b8eef1033
CHEBIR-art <khouloud.chebir@etu.univ-smb.fr>
2024-11-15 17:03:25 +0100

    GPIO and UART ACTIVATION

---
+++ Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h \
+ D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/core_cm0plus.h \
+ D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ ../Inc/main.h
+D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l073xx.h:
+D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/core_cm0plus.h:
+D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/core_cmInstr.h:
+D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Include/core_cmFunc.h:
+D:/Users/user/Documents/STM32CubeIDE/workspace_1.16.1/stm32l073/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+../Inc/main.h:
---
+++ Inc/main.h
@@ -0,0 +1,6 @@
+
+#include <stdint.h>
+#include "stm32l073xx.h"
+
+void GPIO_clock_INIT(void );
+void UART2_INIT(void);
